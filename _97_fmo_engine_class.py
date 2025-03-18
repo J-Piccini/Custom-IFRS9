d2e83d4261d26947e9aeb811a8bd086cd0e67176
Jacopo Piccini <69762352+J-Piccini@users.noreply.github.com>
2025-03-18 16:24:16 +0100

    Update _97_fmo_engine_class.py

--- _97_fmo_engine_class.py
+++ _97_fmo_engine_class.py
@@ -18,9 +18,7 @@ class IFRS9Engine:
         self,
         portfolio_name: Union[str, List[str]],
         reference_snapshot: Union[str, int, bool],
-        test: bool = False,
-        perf_def_split: bool = False,
-        sicr_active: bool = False
+        test: bool = False
     ):
         """
         Initialize IFRS9 Engine with portfolio name(s) and reference snapshot
@@ -37,7 +35,7 @@ class IFRS9Engine:
         self.params_data_prep = _00_fmo_parameters.DataPreparationParameters()
         self.params_application = _00_fmo_parameters.ApplicationParameters()
         self.params_ccf = _00_fmo_parameters.CreditConversionFactorParameters()
-        self.params_risk = _10_fmo_json.load_risk_parameters('./src/risk_parameters3.json')
+        self.params_risk = _10_fmo_json.load_risk_parameters('./src/risk_parameters6.json')
 
         self.marginal_pd_curves_dict =_09_fmo_engine.marginal_pd_arr_set_up(
             risk_params = self.params_risk,
@@ -47,11 +45,8 @@ class IFRS9Engine:
         self.portfolio_list = self.create_portfolio(
             portfolio_name = self.portfolio_name,
             reference_snapshot = reference_snapshot,
-            test = test,
-            perf_def_split = perf_def_split,
-            sicr_active = sicr_active
+            test = test
         )
-
 
     def _defaulted_redeemed_merging(
         self,
@@ -106,9 +101,7 @@ class IFRS9Engine:
         self,
         portfolio_name: str,
         reference_snapshot: Union[str, int],
-        test: bool = False,
-        perf_def_split: bool = False,
-        sicr_active: bool = False
+        test: bool = False
     ) -> Portfolio:
         """
         Create a single portfolio instance
@@ -148,25 +141,23 @@ class IFRS9Engine:
             age = ptf_age,
             exposure_column = exposure_column,
             exposure = ptf_df[exposure_column].sum(),
-            guarantee = 0.,
+            guarantee = self.params_application.guarantee_dict[ptf_df['Portfolio_u'].unique()[0]],
+
             sid = self.params_application.early_sid_dict[ptf_df['Portfolio_u'].unique()[0]],
             scheduled_sid = self.params_application.scheduled_sid_dict[ptf_df['Portfolio_u'].unique()[0]],
             cover_stop_date = self.params_application.cover_stop_date_dict[ptf_df['Portfolio_u'].unique()[0]],
+            covered_interest_bool = self.params_application.covered_interest_dict[ptf_df['Portfolio_u'].unique()[0]],
             
             reference_snapshot = ptf_df,
             ptf_marginal_pd_dict = self.marginal_pd_curves_dict[ptf_df['Geography'].unique()[0]],
-            lgd_arr = self.params_risk['lgd'],
-            perf_def_split = perf_def_split,
-            sicr_active = sicr_active
+            lgd_arr = self.params_risk['lgd']
         )
 
     def create_portfolio(
         self,
         portfolio_name: Union[str, List[str]],
         reference_snapshot: Union[str, int, bool],
-        test: bool = False,
-        perf_def_split: bool = False,
-        sicr_active: bool = False
+        test: bool = False
     ) -> Union[Portfolio, List[Portfolio]]:
         """
         Create portfolio(s) based on input name(s)
@@ -184,9 +175,7 @@ class IFRS9Engine:
                 self._create_single_portfolio(
                     portfolio, 
                     reference_snapshot,
-                    test = test,
-                    perf_def_split = perf_def_split,
-                    sicr_active = sicr_active,
+                    test = test
                 )
                 for portfolio in portfolio_name
             ]
@@ -194,33 +183,50 @@ class IFRS9Engine:
             self._create_single_portfolio(
                 portfolio_name, 
                 reference_snapshot,
-                test = test,
-                perf_def_split = perf_def_split,
-                sicr_active = sicr_active
+                test = test
             )]
-    
 
-    def portfolio_ccf_loop(
+    def new_portfolio_ccf_loop(
         self,
         ptf_obj: Portfolio,
+        sid_threshold: float = 0.1
     ) -> None:
-        """
-        
-        """
 
         ccf_selected = _09_fmo_engine.ccf_selection(
-            ptf_obj.age,
-            ccf_matrix = self.params_risk['ccf'],
+            portfolio_age = ptf_obj.age,
+            ccf_matrix = self.params_risk['K'],
             params = self.params_ccf
         )
+        sid_first_run = self.params_application.early_sid_dict[ptf_obj.portfolio] if isinstance(self.params_application.early_sid_dict[ptf_obj.portfolio], pd.Timestamp) else None
 
-        ptf_obj.cashflow_schedule = ptf_obj.ccf_preprocessing(
-            params = self.params_ccf
+        ptf_obj.cashflow_schedule = ptf_obj.new_ccf_application(
+            ccf_row = ccf_selected,
+            sid_date = sid_first_run,
+            params = self.params_application,
+            lgd = self.params_risk['lgd'][0]
         )
 
-        ptf_obj.cashflow_schedule = ptf_obj.ccf_application(
-            ccf_selected = ccf_selected,
-            marginal_pd_dict = ptf_obj.ptf_marginal_pd_dict,
-            lgd_guarantee = self.params_risk['lgd'][1],
-            params = self.params_application
+        sid_row = ptf_obj.new_sid_check(
+            params_application = self.params_application,
+            ccf_row = ccf_selected,
+            threshold = sid_threshold,
+            lgd = self.params_risk['lgd'][0]
         )
+
+        condition_new_sid_check = (ptf_obj.cashflow_schedule.loc[:sid_row, 'Percentage EL'] > sid_threshold).sum()
+        count = 0
+        while  condition_new_sid_check:
+            loop_sid_row = ptf_obj.new_sid_check(
+                params_application = self.params_application,
+                ccf_row = ccf_selected,
+                threshold = sid_threshold,
+                lgd = self.params_risk['lgd'][0]
+            )
+            condition_new_sid_check = (ptf_obj.cashflow_schedule.loc[:loop_sid_row-1, 'Percentage EL'] > sid_threshold).sum()
+
+            count += 1
+            if count >= 30:
+                print(count)
+                break
+
+
